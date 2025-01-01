87b93721f4acfc7b87cbfa98f03bc4510925745f
Abdullah <babahdigital@gmail.com>
2025-01-01 11:46:57 +0800

    Auto-commit: 2025-01-01_11:46:57

---
+++ tool/enkripsi.sh
@@ -0,0 +1,6 @@
+# credentials.sh
+
+# Informasi kredensial RTSP terenkripsi (Base64)
+DVR_IP_BASE64=$(echo -n "172.16.10.252" | base64)
+RTSP_USER_BASE64=$(echo -n "babahdigital" | base64)
+RTSP_PASSWORD_BASE64=$(echo -n "Admin123@" | base64)
